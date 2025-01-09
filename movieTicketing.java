5391352a60ecf1f7316fcb144b698bb8e4c67dc8
Carlos Miguel Marquez Suela <126050848+carlossuela576@users.noreply.github.com>
2025-01-09 14:04:09 +0700

    commit update
    - improvements
    - case 1, done
    - incomplete

--- movieTicketing.java
+++ movieTicketing.java
@@ -19,26 +19,54 @@ public class movieTicketing{
         System.out.println("10. The Jungle Book");
 
         //user input, movie selection
-        System.out.println("Enter the number of the movie you want to watch:");
+        System.out.print("Enter the number of the movie you want to watch: ");
         int movieChoice = sc.nextInt();
 
         //switch case for movie selection, and seat selection
         switch (movieChoice){
             case 1:
-                System.out.println("You have selected The Lion King");
-                System.out.print("Please select seat row: ");
-                int row1 = sc.nextInt();
-                System.out.print("Please select seat column: ");
-                int col1 = sc.nextInt();
-                //using arrays to create seat data
-                int [][] seatDat1 = {
-                    {0, 1, 0, 0, 1},
-                    {1, 0, 0, 1, 1},
-                    {0, 1, 1, 0, 0},
-                    {1, 1, 1, 0, 1},
-                    {1, 0, 1, 1, 0}
-                };
-                
+                boolean auth1 = true;
+                while(auth1){
+                    System.out.println("You have selected The Lion King");
+                    //output seat data
+                    System.out.println("1 is sold and 0 is available");
+                    System.out.println("-----------------------------");
+                    System.out.println("[0, 1, 0, 0, 1]");
+                    System.out.println("[1, 0, 0, 1, 1]");
+                    System.out.println("[0, 1, 1, 0, 0]");
+                    System.out.println("[1, 1, 1, 0, 1]");
+                    System.out.println("[1, 0, 1, 1, 0]");
+                    System.out.println("-----------------------------");
+                    //user input, seat data
+                    System.out.print("Please select seat row: ");
+                    int row1 = sc.nextInt();
+                    System.out.print("Please select seat column: ");
+                    int col1 = sc.nextInt();
+
+                    //using arrays to create seat data
+                    int [][] seatDat1 = {
+                        {0, 1, 0, 0, 1},
+                        {1, 0, 0, 1, 1},
+                        {0, 1, 1, 0, 0},
+                        {1, 1, 1, 0, 1},
+                        {1, 0, 1, 1, 0}
+                    };
+                    //seat choice verification
+                    int pos1 = seatDat1[row1 - 1][col1 - 1];
+
+                    if (pos1 == 1){
+                        System.out.println("------------------------------");
+                        System.out.println("SOLD, please pick another seat");
+                        System.out.println("------------------------------");
+                    }
+                    else{
+                        System.out.println("------------------------------");
+                        System.out.println("AVAILABLE, please enjoy the movie");
+                        System.out.println("------------------------------");
+                        auth1 = false;
+                    }
+                }
+                break;
         }
     }
 }
