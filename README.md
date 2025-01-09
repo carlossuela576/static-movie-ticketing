89c96c826950e9f00143fc0b21b729ccd6484b3a
Carlos Miguel Marquez Suela <126050848+carlossuela576@users.noreply.github.com>
2025-01-09 12:39:51 +0700

    initial commit
    - creating java file, incomplete
    - update README

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# static movie ticketing
- A basic static system for movie seat ticketing. Project Based Learning 
+# Static Movie Ticketing System
+ Creating a basic static system for movie seat ticketing. Project Based Learning.
+
+ Only meant for educational and learning purposes.  
---
+++ movieTicketing.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class movieTicketing{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
+        //movie list
+        System.out.println("Welcome to the Movie Ticketing System");
+        System.out.println("Please select a movie to watch:");
+        System.out.println("1. The Lion King");
+        System.out.println("2. The Avengers");
+        System.out.println("3. The Incredibles");
+        System.out.println("4. The Little Mermaid");
+        System.out.println("5. The Jungle Book");
+        System.out.println("6. The Lion King");
+        System.out.println("7. The Avengers");
+        System.out.println("8. The Incredibles");
+        System.out.println("9. The Little Mermaid");
+        System.out.println("10. The Jungle Book");
+
+        //user input, movie selection
+        System.out.println("Enter the number of the movie you want to watch:");
+        int movieChoice = sc.nextInt();
+
+        //switch case for movie selection, and seat selection
+        switch (movieChoice){
+            case 1:
+                System.out.println("You have selected The Lion King");
+                
+        }
+    }
+}
